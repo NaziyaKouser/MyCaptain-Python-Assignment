4ce1601a954e42baa4782dbee623d29c7cbe5a73
NaziyaKouser <125481884+NaziyaKouser@users.noreply.github.com>
2023-02-18 20:24:35 +0530

    Create frequency of a string.py

---
+++ frequency of a string.py
@@ -0,0 +1,14 @@
+#Finding a frequency of a string 
+
+def most_frequent(str):
+    d={ }
+    for i in str:
+        if i not in d and i.isalpha():
+            d[i]=0
+        if i in d and i.isalpha():
+            d[i]+=1
+    D=dict(sorted(d.items(),key=lambda x:x[1], reverse=True))
+    for i in D.items():
+        print(i)
+    
+most_frequent('Mississippi')
